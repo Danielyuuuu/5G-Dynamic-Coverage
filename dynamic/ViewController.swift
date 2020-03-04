0dd06326288283c5a931d0945d9cad814bf3a782
Daniel Yu <shenglan.yu@outlook.com>
2020-03-03 22:54:08 -0800

    Initial Commit

---
+++ dynamic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dynamic
+//
+//  Created by shenglanyu on 3/3/20.
+//  Copyright © 2020 shenglanyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
