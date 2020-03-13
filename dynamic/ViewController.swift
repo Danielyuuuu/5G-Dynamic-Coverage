4f2db8182fd93768f76faf7f5b623499eec2cb53
Daniel Yu <shenglan.yu@outlook.com>
2020-03-13 13:59:17 -0700

    second commit: add motion, type

--- dynamic/ViewController.swift
+++ dynamic/ViewController.swift
@@ -8,16 +8,24 @@
 
 import UIKit
 import CoreLocation
+import CoreMotion
 import Firebase
 import MapKit
+import CoreTelephony
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
     let locman = CLLocationManager()
+    let motion = CMMotionManager()
+    
     var location: CLLocation?
     var startTime: Date?
+    var timer: Timer!
+    
+    let hz = 1.0
     
     var ref: DatabaseReference!
+    let id = UIDevice.current.identifierForVendor!.uuidString
     
     @IBOutlet weak var latencyLabel: UILabel!
     
@@ -25,63 +33,109 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         locman.requestAlwaysAuthorization()
         locman.requestWhenInUseAuthorization()
         ref = Database.database().reference()
+        
+        if self.motion.isAccelerometerAvailable {
+            self.motion.accelerometerUpdateInterval = hz
+            self.motion.startAccelerometerUpdates()
+        }
+        
+        if motion.isGyroAvailable {
+            self.motion.gyroUpdateInterval = hz
+            self.motion.startGyroUpdates()
+        }
+        
+        if motion.isMagnetometerAvailable {
+            self.motion.magnetometerUpdateInterval = hz
+            self.motion.startMagnetometerUpdates()
+        }
+        
+        // currently not in use
+        if motion.isDeviceMotionAvailable {
+            self.motion.deviceMotionUpdateInterval = hz
+            self.motion.startDeviceMotionUpdates()
+        }
+        
+        self.timer = Timer(fire: Date(), interval: (hz),
+              repeats: true, block: { (timer) in
+                let motionDict = self.updateMotionData()
+                let networks = self.getNetworkInfo()
+                self.pingFirebase(motionDict: motionDict, networks:networks)
+        })
+        
+        RunLoop.current.add(self.timer!, forMode: .common)
+        
     }
     
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-
-        guard let loc = locations.last else { return }
-
-        let time = loc.timestamp
-
-        guard var startTime = startTime else {
-            self.startTime = time // Saving time of first location, so we could use it to compare later with second location time.
-            return //Returning from this function, as at this moment we don't have second location.
-        }
-
-        let elapsed = time.timeIntervalSince(startTime) // Calculating time interval between first and second (previously saved) locations timestamps.
-
-        if elapsed > 30 { //If time interval is more than 30 seconds
-            print("Upload updated location to server")
-            //updateUser(location: loc) //user function which uploads user location or coordinate to server.
+    func getNetworkInfo() -> [Dictionary<String,String>] {
+        let networkInfo = CTTelephonyNetworkInfo()
+        let networkSims = networkInfo.serviceCurrentRadioAccessTechnology
+        let carrier = networkInfo.serviceSubscriberCellularProviders
+        
+        var networks: [Dictionary<String,String>] = []
+        
+        for sim in networkSims!.keys {
+            let carrierName = carrier![sim]?.carrierName
+            let networkString = networkSims![sim]
+            var dict = ["name": carrierName]
             
-            functions.httpsCallable("helloWorld").call() { (result, error) in
-              if let error = error as NSError? {
-                if error.domain == FunctionsErrorDomain {
-                  //let code = FunctionsErrorCode(rawValue: error.code)
-                  //let message = error.localizedDescription
-                  //let details = error.userInfo[FunctionsErrorDetailsKey]
-                }
-                
-              }
-              if let text = (result?.data as? String) {
-                print(text)
-              }
+            if networkString == CTRadioAccessTechnologyLTE{
+                dict["type"] = "4G"
+            } else if networkString == CTRadioAccessTechnologyWCDMA{
+                dict["type"] = "3G"
+            } else if networkString == CTRadioAccessTechnologyEdge{
+                dict["type"] = "2G"
+            } else {
+                dict["type"] = "unknow"
             }
             
-            startTime = time //Changing our timestamp of previous location to timestamp of location we already uploaded.
-
+            networks.append(dict as! [String : String])
         }
+        
+        return networks
     }
     
-    func ping() {
+    func updateMotionData() -> Dictionary<String, Double> {
+        var dict: [String: Double] = [:]
+        if let data = self.motion.accelerometerData {
+            dict["x_accel"] = data.acceleration.x
+            dict["y_accel"] = data.acceleration.y
+            dict["z_accel"] = data.acceleration.z
+        }
+        
+        if let data = self.motion.gyroData {
+           dict["x_rot"] = data.rotationRate.x
+           dict["y_rot"] = data.rotationRate.y
+           dict["z_rot"] = data.rotationRate.z
+        }
+        
+        if let data = self.motion.magnetometerData {
+            dict["x_mag"] = data.magneticField.x
+            dict["y_mag"] = data.magneticField.y
+            dict["z_mag"] = data.magneticField.z
+        }
+        
+        return dict
+    }
+    
+    func pingFirebase(motionDict:Dictionary<String, Double>, networks:[Dictionary<String,String>]) {
         var currentLocation: CLLocation!
         let now = Date()
+        
         let formatter = DateFormatter()
         formatter.timeZone = TimeZone.current
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
         let dateString = formatter.string(from: now)
+        
         let timer = ParkBenchTimer()
         
         functions.httpsCallable("ping").call() { (result, error) in
           if let error = error as NSError? {
             if error.domain == FunctionsErrorDomain {
-              //let code = FunctionsErrorCode(rawValue: error.code)
-              //let message = error.localizedDescription
-              //let details = error.userInfo[FunctionsErrorDetailsKey]
+              // not handled now
             }
             
           }
@@ -95,15 +149,31 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                CLLocationManager.authorizationStatus() ==  CLAuthorizationStatus.authorizedAlways
             {
                 currentLocation = self.locman.location
-                print(currentLocation.coordinate.longitude, currentLocation.coordinate.latitude)
-                
-                self.ref.child("location").child(dateString).setValue(["longitude": currentLocation.coordinate.longitude, "latitude": currentLocation.coordinate.latitude, "latency": elpased]) {
+            self.ref.child("usrData").child(dateString).child(self.id).setValue(["longitude": currentLocation.coordinate.longitude, "latitude": currentLocation.coordinate.latitude, "latency": elpased]) {
+            
+                    (error:Error?, ref:DatabaseReference) in
+                    if let error = error {
+                        print("Location Data could not be saved: \(error).")
+                    } else {
+                        print("Location Data saved successfully!")
+                    }
+                }
+            }
+            
+        self.ref.child("usrData").child(dateString).child(self.id).updateChildValues(motionDict) {
                 (error:Error?, ref:DatabaseReference) in
                 if let error = error {
-                  print("Data could not be saved: \(error).")
+                    print("Motion Data could not be saved: \(error).")
                 } else {
-                  print("Data saved successfully!")
+                    print("Motion Data saved successfully!")
                 }
+            }
+        self.ref.child("usrData").child(dateString).child(self.id).updateChildValues(["cellularInfo":networks, "latency":elpased]) {
+                (error:Error?, ref:DatabaseReference) in
+                if let error = error {
+                    print("Network Data could not be saved: \(error).")
+                } else {
+                    print("Network Data saved successfully!")
                 }
             }
           }
@@ -111,34 +181,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
 
     @IBAction func dealTapped(_ sender: Any) {
-        //locationManager(locman)
-        ping()
         
-        /*
-        var currentLocation: CLLocation!
-        let now = Date()
-        let formatter = DateFormatter()
-        formatter.timeZone = TimeZone.current
-        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-        let dateString = formatter.string(from: now)
-
-        if
-           CLLocationManager.authorizationStatus() == CLAuthorizationStatus.authorizedWhenInUse ||
-           CLLocationManager.authorizationStatus() ==  CLAuthorizationStatus.authorizedAlways
-        {
-            currentLocation = locman.location
-            print(currentLocation.coordinate.longitude, currentLocation.coordinate.latitude)
-            
-            ref.child("location").child(dateString).setValue(["longitude": currentLocation.coordinate.longitude, "latitude": currentLocation.coordinate.latitude]) {
-              (error:Error?, ref:DatabaseReference) in
-              if let error = error {
-                print("Data could not be saved: \(error).")
-              } else {
-                print("Data saved successfully!")
-              }
-            }
-        }
-        */
     }
 }
 
