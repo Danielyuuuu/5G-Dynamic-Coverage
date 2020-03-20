af7cdf70e8f94e858a91713e3230256840a1a804
Daniel Yu <shenglan.yu@outlook.com>
2020-03-20 09:53:08 -0700

    add main

--- firebase/main.py
+++ firebase/main.py
@@ -30,3 +30,9 @@ def home():
     l = len(data.keys())
     
     return render_template('heatmap.html', co_data = data, co_lenth = l)
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
